b73607cbc2d724f06947396aad389a1b0f237417
Thomas Bewell <tombewell@gmail.com>
2021-11-01 17:24:16 -0500

    create CommentForm component

---
+++ client/src/components/CommentForm/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_COMMENT } from '../../utils/mutations';
+
+const CommentForm = ({ postId }) => {
+    const [commentBody, setBody] = useState('');
+    const [characterCount, setCharacterCount] = useState(0);
+    const [addComment, { error }] = useMutation(ADD_COMMENT);
+
+    // update state based on form inputs
+    const handleChange = event => {
+        if (event.target.value.length <= 280) {
+            setBody(event.target.value);
+            setCharacterCount(event.target.value.length);
+        }
+    };
+
+    // submit form
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            await addComment({
+                variables: { commentBody, postId }
+            });
+
+            // clear form value
+            setBody('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return (
+        <div>
+            <p className={`${characterCount === 280 || error ? 'text-error' : ''}`}>
+                Character Count: {characterCount}/280
+                {error && <span>Something went wrong...</span>}
+            </p>
+            <form
+                onSubmit={handleFormSubmit}
+            >
+                <textarea
+                    placeholder="Leave a comment"
+                    value={commentBody}
+                    onChange={handleChange}
+                ></textarea>
+
+                <button type="submit">
+                    Submit
+                </button>
+            </form>
+
+            {error && <div>Something went wrong...</div>}
+        </div>
+    );
+};
+
+export default CommentForm;
