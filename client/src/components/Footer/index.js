b9cad4b1f86288467215b64ff75cb3b7359c304d
Thomas Bewell <tombewell@gmail.com>
2021-11-01 15:16:40 -0500

    create footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div> 📖 Book-Ends 📘 &copy;2021</div>
+        </footer>
+    );
+};
+
+export default Footer;
